45cbcfc22f4c808af57d49d8d152559e0fae6b28
MoSe <48407417+Merabi1@users.noreply.github.com>
2022-09-03 18:58:10 +0400

    Add project files.

---
+++ Libraries/LightWAP.Core/Domain/Language/Language.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightWAP.Core.Domain.Language
+{
+    public class Language
+    {
+        public string Name { get; set; }
+        public string LanguageCulture { get; set; }
+        public string FlagImageFileName { get; set; }
+        public bool Published { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
