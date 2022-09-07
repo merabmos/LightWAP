37271482cd9ba59bb1939c7422ac615dc08bf3d0
MoSe <48407417+Merabi1@users.noreply.github.com>
2022-09-07 18:36:40 +0400

    Add repository

---
+++ Libraries/LightWAP.Service/Product/ProductService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LightWAP.Services.Product
+{
+    public class ProductService
+    {
+
+    }
+}
---
+++ Libraries/LightWAP.Service/Repository/Repository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using LightWAP.Data;
+
+namespace WebStore.Services.Repository
+{
+    public class Repository<TEntity> where TEntity : class
+    {
+        private readonly LightWAPDBContext _context;
+        private readonly DbSet<TEntity> _table;
+        public Repository(LightWAPDBContext context)
+        {
+            _context = context;
+            _table = _context.Set<TEntity>();
+        }
+    
+        public async Task InsertAsync(TEntity obj)
+        {
+            await _table.AddAsync(obj);
+            await SaveAsync();
+        }
+        public async Task UpdateAsync(TEntity obj)
+        {
+            if (obj != null)
+            {
+                _table.Attach(obj);
+                _context.Entry(obj).State = EntityState.Modified;
+                await SaveAsync();
+            }
+        }
+
+        // Delete Record
+        public async Task DeleteAsync(TEntity existing)
+        {
+            _table.Remove(existing);
+            await SaveAsync();
+        }
+        public async Task<IList<TEntity>> GetAllAsync()
+        {
+            return await _table.ToListAsync();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
